ff5d1263d8e824186c5dcfd884dd39b620ad9f66
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2025-02-24 14:37:13 +1100

    added missing function and updated simulation code

---
+++ R/glmFit.R
@@ -0,0 +1,187 @@
+#' glmFit
+#'
+#' This function fits a ordinalbeta regression model using the `glmmTMB` package.
+#' It takes a `SummarizedExperiment` object as input, along with a user-defined formula,
+#' and fits a zero-inflated beta regression model on the assay data.
+#'
+#' @param se SummarizedExperiment. A `SummarizedExperiment` object containing the assay data and metadata.
+#' @param formula Formula. A formula to specify the fixed and random effects, e.g., ` ~ Group + (1|Sample)`.
+#' @param nthreads An integer specifying the number of (CPUs or workers) to use. Defaults
+#'        to one 1.
+#' @param family A `glmmTMB` family object. Defaults to `glmmTMB::ordbeta()`.
+#' @param BPPARAM Optional `BiocParallelParam` object. If not provided, the function
+#'        will configure an appropriate backend automatically.
+#'
+#' @return A list with the following components:
+#' \item{coefficients}{A data frame with coefficients, standard errors, z-values, p-values, and FDR for each feature.}
+#' \item{fit}{The fitted `glmmTMB` model object.}
+#' \item{call}{The original function call.}
+#'
+#' @import SummarizedExperiment
+#' @importFrom glmmTMB glmmTMB
+#'
+#' @examples
+#' \dontrun{
+#' library(SummarizedExperiment)
+#' library(strainspy)
+#' library(glmmTMB)
+#'
+#' example_meta_path <- system.file("extdata", "example_metadata.csv.gz", package = "strainspy")
+#' example_meta <- readr::read_csv(example_meta_path)
+#' example_path <- system.file("extdata", "example_sylph_profile.tsv.gz", package = "strainspy")
+#' se <- read_sylph(example_path, example_meta)
+#' se <- filter_by_presence(se)
+#'
+#' design <- as.formula(" ~ Case_status + Age_at_collection")
+#'
+#' fit <- glmFit(se,  design, nthreads=4, family=glmmTMB::ordbeta())
+#' summary(fit)
+#'
+#' }
+#'
+#' @export
+glmFit <- function(se, design, nthreads=1, scale_continous=TRUE, family=glmmTMB::ordbeta(), BPPARAM=NULL) {
+  # Check if glmmTMB is installed
+  if (!requireNamespace("glmmTMB", quietly = TRUE)) {
+    stop("The 'glmmTMB' package is required but is not installed. Please install it with install.packages('glmmTMB').")
+  }
+
+  # Validate input
+  if (!inherits(se, "SummarizedExperiment")) {
+    stop("`se` must be a SummarizedExperiment object.")
+  }
+
+  # colData (sample metadata)
+  col_data <- colData(se)
+  if (scale_continous==TRUE){
+    for (col in names(col_data)) {
+      if (is.numeric(col_data[[col]])) {
+        col_data[[col]] <- scale(col_data[[col]])  # Scale numeric columns
+      }
+    }
+  }
+
+  # Ensure the design is a formula
+  if (!inherits(design, "formula")) {
+    stop("`design` must be a formula (e.g., ~ batch + condition).")
+  }
+  combined_formula <- as.formula(paste("Value", deparse(design), sep = " "))
+
+  # Ensure that rownames of colData match colnames of the assay
+  if (!all(colnames(assays(se)[[1]]) %in% rownames(col_data))) {
+    stop("Column names of assay data do not match row names of colData.")
+  }
+
+  # Define priors
+  nbeta <- ncol(model.matrix(design, col_data))
+  fixed_priors <- data.frame(
+    prior = rep("normal(0,5)", nbeta),
+    class = rep("fixef", each=nbeta),
+    coef  = as.character(seq(1,nbeta)))
+
+  # Set up parallel infrastructure
+  if ((nthreads > 1) & (.Platform$OS.type != "windows")) {
+    # Check the operating system and set the backend accordingly
+    if (is.null(BPPARAM)) {
+      # Use MulticoreParam for Unix-like systems
+      # BPPARAM <- BiocParallel::MulticoreParam(
+      #   workers = nthreads
+      # )
+      BPPARAM <- BiocParallel::SnowParam(workers = nthreads, progressbar = TRUE, tasks=100)
+    }
+  } else {
+    BPPARAM <- BiocParallel::SerialParam(progressbar = TRUE)
+  }
+
+  # Split rows into 50-row chunks
+  row_chunks <- split(
+    seq_len(nrow(se)),
+    ceiling(seq_len(nrow(se)) / 100)  # 50 rows per chunk
+  )
+
+  results <- BiocParallel::bplapply(
+    row_chunks,
+    function(row_indices) fit_model(SummarizedExperiment::assay(se)[row_indices,],
+                                    col_data, combined_formula, fixed_priors,
+                                    family),
+    BPPARAM = BPPARAM
+  )
+
+  # Flatten the results by removing the first layer of lists
+  results <- do.call(c, results)
+
+  # Clean up
+  BiocParallel::bpstop(BPPARAM)
+
+  # Create the betaGLM object
+  ZIBetaGLM <- new("betaGLM",
+                   row_data = rowData(se),
+                   coefficients = DataFrame(purrr::map_dfr(results, ~ .x[[1]][,1])),
+                   std_errors = DataFrame(purrr::map_dfr(results, ~ .x[[1]][,2])),
+                   p_values = DataFrame(purrr::map_dfr(results, ~ .x[[1]][,4])),
+                   zi_coefficients = NULL,
+                   zi_std_errors = NULL,
+                   zi_p_values = NULL,
+                   residuals = DataFrame(purrr::map_dfr(results, ~ .x[[3]])),
+                   design = model.matrix(design, data = as.data.frame(colData(se))),
+                   # assay = assays(se)[[1]],  # Retrieve assay data matrix from SummarizedExperiment
+                   call = match.call()  # Store the function call for reproducibility
+  )
+
+  return(ZIBetaGLM)
+}
+
+
+#' Fit Zero-Inflated Beta Regression for a Single Feature
+#'
+#' Fits a zero-inflated beta regression for a single feature in an assay
+#' using `glmmTMB`.
+#'
+#' @param se A `SummarizedExperiment` object containing the assay data.
+#' @param row_index The index of the feature (row) to be processed.
+#' @param col_data A data frame containing the design matrix and additional covariates.
+#' @param combined_formula The formula for the conditional mean model.
+#' @param fixed_priors Optional priors for the model.
+#' @param nt Number of threads for parallel computation in model fitting.
+#' @param feature Optional name of the feature for debugging or error messages.
+#' @return A list with model coefficients, zero-inflation coefficients, residuals,
+#'         log-likelihood, and convergence status.
+fit_model <- function(se_subset, col_data, combined_formula, fixed_priors, family) {
+
+  chunk_results <- lapply(seq_len(nrow(se_subset)), function(row_index){
+    # Extract the values for the current feature
+    col_data$Value <- base::pmin(as.vector(se_subset[row_index, ]) / 100, 0.99999)
+
+    # Run the zero-inflated beta regression
+    fit <- tryCatch({
+      glmmTMB::glmmTMB(
+        formula = combined_formula,
+        data = col_data,
+        priors = fixed_priors,
+        family = family
+      )
+    }, error = function(e) NULL)
+
+    # Handle the case where the model could not be fitted
+    if (is.null(fit)) {
+      warning("Failed to fit the model for species index: ", row_index)
+      return(NULL)
+    }
+
+    # Extract summary statistics
+    smry <- summary(fit)
+
+    # Return results as a list
+    return(list(
+      coefficients = smry$coefficients$cond,
+      coefficients_zi = NULL,
+      residuals = residuals(fit, type = "response"),
+      log_likelihood = fit$fit$objective,
+      convergence = fit$fit$convergence
+    ))
+  })
+
+  return(chunk_results)
+}
+
+
--- R/rescale_beta.R
+++ R/rescale_beta.R
@@ -1,44 +1,74 @@
-#' Simulate and Analyze Beta-Distributed Data with a Treatment Effect
+#' Transform ANI Data to Simulate a Treatment Effect
 #'
 #' This function applies a simple linear transformation to simulate a treatment effect.
 #'
 #' @param x a vector of sequence identity values to transform
-#' @param lambda the power to apply to the transformation
+#' @param beta simulate a decreased in identiy by factor of beta (default=0.95)
+#' @param zi simulate an increased shift in the proportion of zeros by zi (default=0.1)
 #' @return a vector of transformed values
 #' @examples
 #' N_group <- 1000
-#' difference <- runif(1, 0.5, 0.9)
 #'
 #' shape1 <- 2
 #' shape2 <- 10
+#' base_zero <- 0.5
 #'
 #' sim <- rbeta(N_group, shape1, shape2)
+#' sim <- sim*rbinom(length(sim), 1, base_zero)
+#'
+#'
+#' # Simulate a treatment effect.
+#'
+#' beta_difference <- runif(1, 0.5, 0.9)
+#' treat_zero_effect <- 0.2
+#'
+#' treat <- rescale_beta(sim, beta_difference, treat_zero_effect)
 #'
 #' df <- data.frame(cat=rep(c('a','b'), each=N_group),
-#'                  vals=c(sim,
-#'                         rescale_beta(sim, difference)))
+#'                  vals=c(sim,treat$rescaled))
 #'
 #' m <- glmmTMB::glmmTMB(vals ~ cat, data=df,
-#'                       family=glmmTMB::beta_family(link="logit"))
-#' coefs <- glmmTMB::fixef(m)$cond
+#'                       family=glmmTMB::beta_family(link="logit"),
+#'                       ziformula = ~cat)
 #'
-#' cat("Simulated log-odds", log(difference), "\n",
-#'     "Inferred log-odds", coefs["catb"])
+#' coefs <- glmmTMB::fixef(m)$cond
+#' zi_coefs <- glmmTMB::fixef(m)$zi
+#'
+#' cat("Simulated log-odds ratio", log(beta_difference), "\n",
+#'     "Inferred log-odds ratio", coefs["catb"])
+#'
+#' cat("Simulated log-odds ratio", treat$expected_zi, "\n",
+#'     "Inferred log-odds ratio", zi_coefs["catb"])
+#'
 #' @export
-rescale_beta <- function(x, lambda = 0.5) {
+rescale_beta <- function(x, beta = 0.95, zi=0.1) {
+
   # Ensure x is between 0 and 1
   if (any(x < 0 | x > 1)) stop("Values must be between 0 and 1")
-  # Ensure lambda is between 0 and 1
-  if (any(lambda < 0 | lambda > 1)) stop("lambda must be between 0 and 1")
+  # Ensure beta is between 0 and 1
+  if (any(beta < 0 | beta > 1)) stop("beta must be between 0 and 1")
 
   # Apply power transformation (Beta regression-style scaling)
-  x_rescaled <- x*lambda
+  x_rescaled <- x*beta
 
-  # Calculate expected difference to be inferred after transformation
-  expected_difference <-  beta(shape1 + difference, shape2)/
-       beta(shape1, shape2) - shape1/(shape1+shape2)
+  p1 <- sum(x_rescaled==0)/length(x_rescaled)
+  p2 <- p1 + zi
 
-  return(x_rescaled)
+  if (p2 > 1) {
+    stop("ZI is too large. The total proportion of
+                   zeros is greater than 1 after adjustment")
+  }
+
+  x_rescaled[
+    sample(which(x_rescaled!=0),
+           size=rbinom(1, length(x_rescaled), p2) - sum(x_rescaled==0),
+           replace=FALSE)] <- 0
+
+  return(list(
+    rescaled=x_rescaled,
+    expected_beta=log(beta),
+    expected_zi=log(p2/(1-p2)/(p1/(1-p1)))
+  ))
 }
 
 
